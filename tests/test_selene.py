d834737ccbbce92477c2331dd96c4962c0e626a3
FurmanovaYA <ula55509@mail.ru>
2025-03-16 14:49:54 +0300

    Скорректировала тест

--- tests/test_selene.py
+++ tests/test_selene.py
@@ -1,15 +1,15 @@
-from selene import browser, have,  by , be
+from selene import browser, have, by, be
 from selene.support.shared.jquery_style import s
 
 
+lenedef test_se():
+    browser.open("https://github.com")
+    browser.driver.fullscreen_window()
+    s('.search-input').click()
+    s('#query-builder-test').send_keys("eroshenkoam/allure-integration-example").press_enter()
 
-browser.open("https://github.com")
-browser.driver.fullscreen_window()
-s('.search-input').click()
-s('#query-builder-test').send_keys("eroshenkoam/allure-integration-example").press_enter()
+    s(by.link_text("eroshenkoam/allure-integration-example")).click()
 
-s(by.link_text("eroshenkoam/allure-integration-example")).click()
+    s("#issues-tab").click()
 
-s("#issues-tab").click()
-
-s(by.text('No results')).should(be.visible)
+    s(by.text('No results')).should(be.visible)
